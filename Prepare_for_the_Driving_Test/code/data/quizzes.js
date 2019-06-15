574a2b76b92edb13dc05ec8063f3be2c42fb2a3c
Jamie Dawson <jadawson@e1z3r6p8.42.us.org>
2019-06-14 19:29:07 -0700

    Now has 20 questions

--- Prepare_for_the_Driving_Test/code/data/quizzes.js
+++ Prepare_for_the_Driving_Test/code/data/quizzes.js
@@ -46,12 +46,76 @@ module.exports = [
         question: "What is the minimum tread depth for front tires?",  //QUESTION 9
         options: ["1/32 inch", "4/32 inch", "2/32 inch"],
         answer: 1,
-      },   
+      },
+  
+                
       {
         question: "Which of the following should you NOT do in the event of hydraulic brake failure?",  //QUESTION 10
         options: ["Shift up", "Use parking or emergency break", "Pump the breaks"],
         answer: 0,
-      },                 
+      },  
+    
+        {
+        question: "When two vehicles meet on a steep mountain road where neither can pass, which vehicle has the right-of-way?",  //QUESTION 11
+        options: ["The vehicle traveling downhill", "The vehicle traveling uphill", "Both vehicles"],
+        answer: 1,
+      },              
+                
+        {
+        question: "At an unmarked crosswalk, you must",  //QUESTION 12
+        options: ["Increase your speed and cross before the pedestrians", "yield the right-of-way to pedestrians", "Stop and ask pedestrains to cross quickly"],
+        answer: 1,
+      },   
+                
+      {
+        question: "A traffic signal with a flashing yellow arrow means that you should",  //QUESTION 13
+        options: ["Yield and then proceed with caution", "Stop and prepare to obey the next signal", "Proceed with caution but turns are not permitted in the direction of the arrow"],
+        answer: 0,
+      },             
+        
+                
+        {
+        question: "On a two-lane undivided highway, the maximum speed limit for vehicles towing trailers is ______ unless otherwise posted.",  //QUESTION 14
+        options: ["55 mph", "65 mph", "60 mph"],
+        answer: 0,
+      },                
+                
+         {
+        question: "If your parked car rolls away and hits another unattended vehicle, you should",//QUESTION 15
+        options: ["Report the incident to police", "Remove your car and leave", "Sound your hor to attract attention"],
+        answer: 0,
+      },    
+                
+      {
+        question: "A driver who is involved in a significant collision must report it to the DMV within",//QUESTION 16
+        options: ["15 days", "20 days", "10 days"],
+        answer: 2,
+      },                  
+        
+        {
+        question: "What is the first thing you should do during an enforcement stop?",//QUESTION 17
+        options: ["Turn on your right turn signal", "Stop on the left shoulder", "Stop on the center median"],
+        answer: 0,
+      },                
+                
+      {
+        question: "Use __________ steering when correcting a skid.",//QUESTION 18
+        options: ["hand-by-hand", "one-hand", "hand-over-hand"],
+        answer: 2,
+      },             
+  
+      {
+        question: "A steady red arrow signal means",//QUESTION 19
+        options: ["You must stop and you cannot turn in the direction of the arrow", "You should prepare to stop and yield to oncoming traffic before turning", "You must stop and then proceed when it is safe."],
+        answer: 0,
+      },                
+                
+         {
+        question: "If you are having vehicle trouble and you need to stop, what is the first thing you should do?",//QUESTION 20
+        options: ["Stop your vehicle over a hill or just around the curve for visibility", "Lift the hood to signal an emergency", "Pull off the road and away from traffic."],
+        answer: 0,
+      },              
+                
     ]
   },
 ]
